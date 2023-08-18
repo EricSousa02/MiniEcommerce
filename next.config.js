fd21e2ea51f2cb55b97de9c362ab22114f00e8eb
EricSousa02 <ericsousadeandrade@gmail.com>
2023-08-17 21:57:41 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true
+    },
+    images: {
+        remotePatterns: [
+            {
+            hostname: "images.unsplash.com"
+            },
+            {
+            hostname: "lh3.googleusercontent.com"    
+            },
+            {
+            hostname: "unsplash.com"    
+            },
+            {
+            hostname: "www.google.com"    
+            }
+            
+            
+    ]
+    }
+}
 
 module.exports = nextConfig
